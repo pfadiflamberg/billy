fa48840ce1301aad2c0328321ad44bb7bc6a4266
MrQweep <nicob99@gmx.ch>
2020-08-30 13:24:20 +0000

    added relationships and representation

--- billy/model.py
+++ billy/model.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sa
 from sqlalchemy import create_engine
 import sqlalchemy.dialects.postgresql as sapsql
+from sqlalchemy.orm import relationship
 
 engine_string = "postgresql+psycopg2://postgres:password@billy-postgres:5432/billy"
 engine = create_engine(engine_string,echo=True)
@@ -24,6 +25,13 @@ class BulkInvoice(Base):
     create_time = sa.Column(sa.TIMESTAMP, server_default=sa.func.now(), nullable=False)
     update_time = sa.Column(sa.TIMESTAMP, server_default=sa.func.now(), nullable=False)
 
+    invoices = relationship("Invoice", back_populates = "bulk_invoice")
+
+    def __repr__(self):
+        return "<BulkInvoice(id=%s, state=%s, issuing_date=%s, due_date=%s, len(invoices)=%s, create_time=%s, update_time=%s, text_invoice=%s(...), text_reminder=%s(...))>" % (
+                                self.id, self.state, self.issuing_date, self.due_date, len(self.invoices), self.create_time, self.update_time, str(self.text_invoice)[0:5], str(self.text_reminder)[0:5])
+
+
 class Invoice(Base):
     __tablename__ = "invoice"
 
@@ -40,3 +48,8 @@ class Invoice(Base):
     create_time = sa.Column(sa.TIMESTAMP, server_default=sa.func.now(), nullable=False)
     update_time = sa.Column(sa.TIMESTAMP, server_default=sa.func.now(), nullable=False)
     
+    bulk_invoice = relationship("BulkInvoice", back_populates="invoices")
+
+    def __repr__(self):
+        return "<Invoice(id=%s, state=%s, state_message=%s(...), recipient=%s, recipient_name=%s, bulk_invoice=%s, create_time=%s, update_time=%s)>" % (
+                                self.id, self.state, str(self.state_message)[0:5], self.recipient, self.recipient_name, self.bulk_invoice_id, self.create_time, self.update_time)
