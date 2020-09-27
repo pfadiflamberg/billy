4d7d9a05965e2f57a929d4be3d9483ec95982074
Seiterle <a.seiterle@me.com>
2020-09-27 14:01:08 +0200

    add reactstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button color="danger">Danger!</Button>
       </header>
     </div>
   );
