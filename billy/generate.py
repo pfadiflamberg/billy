f372fa88ff4836ee55d37e26c4fdf82ce868e304
Seiterle <a.seiterle@me.com>
2020-10-18 13:32:24 +0200

    minor fixes

--- billy/generate.py
+++ billy/generate.py
@@ -23,7 +23,7 @@ def add_multiline_text(dwg, text, insert=(0, 0), font_size='14px', font_family=N
         ))
 
 
-def bill(account, creditor, hitobito_debtor, hitobito_back, ref, amount):
+def bill(text_body, account, creditor, hitobito_debtor, hitobito_sender, ref, amount):
     """
     Generate returnes the PDF of a bill.
     """
@@ -46,70 +46,66 @@ def bill(account, creditor, hitobito_debtor, hitobito_back, ref, amount):
     xr = 30
     xl = 460
     y = 200
-    d = 1
-    l = hitobito_back['addr'].split('\n')
+    # Address block
+    # Return address, splitted over two lines
+    l = hitobito_sender['addr'].split('\n')
+    send_back_addr_delimiter = 1
     dwg.add(dwg.text(
-        ', '.join(l[:d]),
+        ', '.join(l[:send_back_addr_delimiter]),
         insert=(xl, y)
     ))
     dwg.add(dwg.text(
-        ', '.join(l[d:]),
+        ', '.join(l[send_back_addr_delimiter:]),
         insert=(xl, y+15)
     ))
+    # PP image by Swiss Post
     dwg.add(dwg.image(
         href='resources/images/pp.png',
         insert=(xl, y + 20),
         size=(234, 65),
     ))
+    # recipient address
     add_multiline_text(dwg, hitobito_debtor['addr'],
                        insert=(xl, y+65),
                        font_size='16px',
                        font_family="helvetica"
                        )
+    # header
+    # header text
+    add_multiline_text(dwg,
+                       '\n'.join([
+                           'Pfadfinderkorps Flamberg, Kasse und Versicherung',
+                           hitobito_sender['addr'].replace('\n', ', '),
+                           'cassa@flamberg.ch, www.flamberg.ch',
+                       ]),
+                       insert=(xr, 50),
+                       font_size='10px',
+                       font_family='helvetica',
+                       )
+    # Logo
     dwg.add(dwg.image(
         href='resources/images/logo.png',
         insert=(550, 40),
         size=(150, 60),
     ))
-    add_multiline_text(dwg,
-                       '\n'.join([
-                           'Pfadfinderkorps Flamberg, Kasse und Versicherung',
-                           hitobito_back['addr'].replace('\n', ', '),
-                           'cassa@flamberg.ch, www.flamberg.ch',
-                       ]),
-                       insert=(xr, 50),
-                       font_size='10px',
-                       font_family='14px',
-                       )
     # add title
     dwg.add(dwg.text(
         'Jahresrechnung',
         font_size='18px',
         insert=(xr, 300)
     ))
-    add_multiline_text(dwg,
-                       """
-        Jedes Jahr erhalten alle Stufen, jeder Harst und Zug einen Beitrag aus der Korpskasse, um Anschaffungen
-        von Material und andere Ausgaben zu finanzieren. Ihr Sohn ist während den Übungen und Lagern, sowie bei
-        der Hin- und Rückfahrt zu Pfadianlässen subsidiär gegen Unfall versichert. Für die Pfadileiter besteht
-        zudem eine Haftpflichtversicherung. Um diese Kosten decken zu können, sind wir auf den Jahresbeitrag je-
-        des einzelnen Flambergers angewiesen.
-        Leider mussten wir feststellen, dass der Jahresbeitrag von Leandro für das Jahr 2019 noch nicht bezahlt
-         wurde. Wir bitten Sie, den Jahresbeitrag von CHF 50.00 in den nächsten 10 Tagen auf unser Bankkonto
-         einzuzahlen. Bitte benützen Sie dazu ausschliesslich den untenstehenden Einzahlungsschein (mehrere
-         Jahresbeiträge bitte jeweils separat). Beim eBanking bitten wir Sie, die Referenznummer unbedingt
-         aufzuführen. Sie erleichtern uns damit die Arbeit wesentlich. Besonders dankbar sind wir denjenigen,
-         die den Betrag grosszügig aufrunden.
-        Sollte Ihr Sohn nicht mehr Mitglied unseres Korps sein und diesen Brief irrtümlicherweise erhalten
-        haben, so bitten wir Sie höflich, dies dem Korpskassier (Adresse siehe oben) schriftlich oder per Email
-        mitzuteilen.
-        Wir danken Ihnen herzlich für das uns entgegengebrachte Vertrauen und wünschen Ihrem Sohn ein ausgefülltes,
-        """,
+    add_multiline_text(dwg, text_body,
                        insert=(xr, 350),
-                       font_family='14px',
+                       font_size='14px',
                        )
     dwg.saveas(TMP_SVG_FILE)
     # convert svg to pdf
-    drawing = svg2rlg(TMP_SVG_FILE)
-    renderPDF.drawToFile(drawing, "tmp.pdf")
-    # return renderPDF.drawToString(drawing)
+    return svg2rlg(TMP_SVG_FILE)
+
+
+def billAsFile(drawing, file):
+    renderPDF.drawToFile(drawing, file)
+
+
+def billAsString(drawing):
+    return renderPDF.drawToString(drawing)
