3f015288dccb8c4ea96477a81d6aad2a6a62aabb
Seiterle <a.seiterle@me.com>
2021-05-18 16:48:36 +0200

    frontend wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
+import {useAppDispatch} from "./app/hooks";
+import {selfConfigure} from "./features/backend/backendSlice";
+import {BulkInvoiceList} from "./pages/BulkInvoiceList";
+
 function App() {
+
+  // self configure backend
+  const dispatch = useAppDispatch();
+  dispatch(selfConfigure());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <BulkInvoiceList />
     </div>
   );
 }
--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import backendReducer from '../features/backend/backendSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    backend: backendReducer,
   },
 });
 
