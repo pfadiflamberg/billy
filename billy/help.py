d2bfe01af04e717dbc65fdab2c96c9d02544b77b
Seiterle <a.seiterle@me.com>
2021-07-31 13:52:40 +0200

    fix

--- billy/help.py
+++ billy/help.py
@@ -3,6 +3,6 @@ import os
 
 def getenv(name):
     value = os.getenv(name)
-    if len(value) == 0:
+    if not value or len(value) == 0:
         raise Exception("missing environment variable: " + name)
     return value
